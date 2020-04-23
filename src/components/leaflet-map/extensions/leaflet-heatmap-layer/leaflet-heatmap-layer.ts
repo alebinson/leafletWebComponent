1f9c866eba0e4b4eb06514633237eb3be68fbd04
alebinson <newdarkvirus@gmail.com>
2020-04-23 17:13:04 +0300

    doc as comment added

--- src/components/leaflet-map/extensions/leaflet-heatmap-layer/leaflet-heatmap-layer.ts
+++ src/components/leaflet-map/extensions/leaflet-heatmap-layer/leaflet-heatmap-layer.ts
@@ -1,5 +1,28 @@
 //inspired by https://github.com/Leaflet/Leaflet.heat/blob/gh-pages/src/HeatLayer.js
 
+// USAGE
+// L.heatLayer(latlngs, options)
+// Constructs a heatmap layer given an array of points and an object with the following options:
+
+//     minOpacity - the minimum opacity the heat will start at
+//     maxZoom - zoom level where the points reach maximum intensity (as intensity scales with zoom), equals maxZoom of the map by default
+//     max - maximum point intensity, 1.0 by default
+//     radius - radius of each "point" of the heatmap, 25 by default
+//     blur - amount of blur, 15 by default
+//     gradient - color gradient config, e.g. {0.4: 'blue', 0.65: 'lime', 1: 'red'}
+
+// Each point in the input array can be either an array like [50.5, 30.5, 0.5], or a Leaflet LatLng object.
+
+// Optional third argument in each LatLng point (altitude) represents point intensity. Unless max option is specified, intensity should range between 0.0 and 1.0.
+
+// Methods
+//     setOptions(options): Sets new heatmap options and redraws it.
+//     addLatLng(latlng): Adds a new point to the heatmap and redraws it.
+//     setLatLngs(latlngs): Resets heatmap data and redraws it.
+//     redraw(): Redraws the heatmap.
+
+
+
 export function extendHeatLayer(L) {
     (function (L, window, document) {
 
